d4d450a3c13242c6b726e6b8b8f2694221d576fa
t3h2mas <t.homas.noee@gmail.com>
2013-09-22 20:04:03 -0600

    init

---
+++ spec/freqspec.js
@@ -0,0 +1,33 @@
+var WordFreq = require('../wordfreq');
+var list = ['esp', 'thomas', 'sup'];
+var Words = new WordFreq(list);
+
+describe("Make the app", function () {
+	it("Words should be an instance of WordFreq", function () {
+		expect(Words instanceof WordFreq).toBe(true);
+	});
+});
+
+describe("Get Test", function () {
+	it("Words.get()", function () {
+		expect(Words.get('thomas')).not.toBe(true);
+	});
+});
+
+describe("Get List", function () {
+	it("List should return a list of Word objects", function() {
+		expect(Words.list().length).toBe(3); // from list /\
+	});
+});
+
+describe("Set -> Add a new entry", function () {
+	it("Should add an entry", function () {
+		expect(Words.set('hay')).not.toBeDefined();
+	});
+	it("Should still be an instance of WordFreq", function () {
+		expect(Words instanceof WordFreq).toBe(true);
+	});
+	it("Should return a result after .get('hay')", function () {
+		expect(Words.get('hay')['word']).toEqual('hay');
+	});
+});
