2f346159fabf683df1080637f63946dae3e8a37b
Thomas Noe <thomas.noe@madwiremedia.com>
2018-05-03 09:02:50 -0600

    move to ava / give circle junit xml

--- test/test.js
+++ test/test.js
@@ -1,63 +1,57 @@
 'use strict'
 
-const expect = require('chai').expect
+const test = require('ava')
 const WordFreq = require('../wordfrequenter.js')
 
-const list = ['foo', 'bar', 'baz']
+const bootstrapWords = () => new WordFreq(['foo', 'bar', 'baz'])
 
-describe('WordFreq', function () {
-    let Words
-    beforeEach(() => {
-        Words = new WordFreq(list)
-    })
-    it('should handle both arrays and strings as input', function () {
-        const fromStr = new WordFreq('foo bar baz')
-        const fromArr = new WordFreq(['foo', 'bar', 'baz'])
+test('should handle both arrays and strings as input', t => {
+    const fromStr = new WordFreq('foo bar baz')
+    const fromArr = new WordFreq(['foo', 'bar', 'baz'])
 
-        expect(fromStr).to.deep.equal(fromArr)
-    })
-    it('should throw when not passed a string or array on initialization', function() {
-        expect(() => new WordFreq(1336)).to.throw('typeof start (number) invalid. must be string or array')
-    })
-    describe('.get', function () {
-        it('should not find a word it was not initialized with', function () {
-            expect(Words.get('ham')).to.equal(undefined)
-        })
-        it('should return the correct word & count', function () {
-            const fooWord = Words.get('foo')
-            expect(fooWord['word']).to.equal('foo')
-            expect(fooWord['count']).to.equal(1)
-        })
-    })
-    describe('.list', function () {
-        it('should have the correct length', function() {
-            expect(Words.list().length).to.equal(3) 
-        })
-        it('should be sorted, most to least, by count', function() {
-            Words.set('foop')
-            Words.set('foop')
-            Words.set('foop')
+    t.deepEqual(fromStr, fromArr)
+})
+test('should throw when not passed a string or array on initialization', t => {
+    t.throws(() => new WordFreq(1336))
+})
+test('.get should not find a word it was not initialized with', t => {
+    const Words = bootstrapWords()
+    t.is(Words.get('ham'), undefined)
+})
+test('.get should return the correct word & count', t => {
+    const Words = bootstrapWords()
+    const fooWord = Words.get('foo')
+    t.is(fooWord['word'], 'foo')
+    t.is(fooWord['count'], 1)
+})
+test('.list should have the correct length', t => {
+    const Words = bootstrapWords()
+    t.is(Words.list().length, 3)
+})
+test('.list should be sorted, most to least, by count', t => {
+    const Words = bootstrapWords()
+    Words.set('foop')
+    Words.set('foop')
+    Words.set('foop')
 
-            Words.set('doop')
-            Words.set('doop')
-            Words.set('doop')
-            Words.set('doop')
-            Words.set('doop')
+    Words.set('doop')
+    Words.set('doop')
+    Words.set('doop')
+    Words.set('doop')
+    Words.set('doop')
 
-            const list = Words.list()
+    const list = Words.list()
 
-            // shoutout: https://gist.github.com/yorkie/7959685#gistcomment-2309147
-            const isSorted = list.map(x => x.count).every((val, i, arr) => !i || (val <= arr[i-1]))
-            expect(isSorted).to.equal(true)
-        })
-    })
-    describe('.set', function () {
-        it('should add a word with a count of 1', function () {
-            expect(Words.set('ni')).to.deep.equal({word: 'ni', count: 1})
-        })
-        it('should increment the count of a word when set twice', function() {
-            Words.set('ni')
-            expect(Words.set('ni').count).to.equal(2)
-        })
-    })
+    // shoutout: https://gist.github.com/yorkie/7959685#gistcomment-2309147
+    const isSorted = list.map(x => x.count).every((val, i, arr) => !i || (val <= arr[i-1]))
+    t.truthy(isSorted)
+})
+test('.set should add a word with a count of 1', t => {
+    const Words = bootstrapWords()
+    t.deepEqual(Words.set('ni'), {word: 'ni', count: 1})
+})
+test('.set should increment the count of a word when set twice', t => {
+    const Words = bootstrapWords()
+    Words.set('ni')
+    t.is(Words.set('ni').count, 2)
 })
