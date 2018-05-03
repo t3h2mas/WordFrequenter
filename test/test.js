8cadc6e5e407e121d94d7aa2b5a3fddad952a46d
Thomas Noe <thomas.noe@madwiremedia.com>
2018-05-02 21:49:14 -0600

    improve .set & .list tests
    set makes sure it increments more than once,
    list checks whether the result is sorted

--- test/test.js
+++ test/test.js
@@ -1,9 +1,13 @@
 const expect = require('chai').expect
 const WordFreq = require('../wordfrequenter.js')
+
+let Words
 const list = ['foo', 'bar', 'baz']
-const Words = new WordFreq(list)
 
 describe('WordFreq', function () {
+    beforeEach(() => {
+        Words = new WordFreq(list)
+    })
     it('Words should be an instance of WordFreq', function () {
         expect(Words instanceof WordFreq).to.equal(true)
     })
@@ -21,11 +25,31 @@ describe('WordFreq', function () {
         it('should have the correct length', function() {
             expect(Words.list().length).to.equal(3) 
         })
-    })
-    describe('.set', function () {
-        it('should add a word w/o returning a value', function () {
-            expect(Words.set('hay')).to.equal(undefined)
+        it('should be sorted, most to least, by count', function() {
+            Words.set('foop')
+            Words.set('foop')
+            Words.set('foop')
+
+            Words.set('doop')
+            Words.set('doop')
+            Words.set('doop')
+            Words.set('doop')
+            Words.set('doop')
+
+            const list = Words.list()
+
+            // shoutout: https://gist.github.com/yorkie/7959685#gistcomment-2309147
+            const isSorted = list.map(x => x.count).every((val, i, arr) => !i || (val <= arr[i-1]))
+            expect(isSorted).to.equal(true)
         })
     })
-
+    describe('.set', function () {
+        it('should add a word with a count of 1', function () {
+            expect(Words.set('ni')).to.deep.equal({word: 'ni', count: 1})
+        })
+        it('should increment the count of a word when set twice', function() {
+            Words.set('ni')
+            expect(Words.set('ni').count).to.equal(2)
+        })
+    })
 })
