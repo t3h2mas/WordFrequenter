39f734a46b446af62cd61a2958036c808dc0ca3b
Thomas Noe <thomas.noe@madwiremedia.com>
2018-05-02 20:37:50 -0600

    use single quotes in test

--- test/test.js
+++ test/test.js
@@ -3,27 +3,27 @@ const WordFreq = require('../wordfrequenter.js')
 const list = ['esp', 'jtruant', 'sup']
 const Words = new WordFreq(list)
 
-describe("WordFreq", function () {
-	it("Words should be an instance of WordFreq", function () {
-		expect(Words instanceof WordFreq).to.equal(true)
-	})
-	describe(".get", function () {
-		it("should not find a word it was not initialized with", function () {
-			expect(Words.get('thomas')).not.to.equal(true)
-		})
-		it("should return the correct word value", function () {
-			expect(Words.get('sup')['word']).to.equal('sup')
-		})
-	})
-	describe(".list", function () {
-		it("should have the correct length", function() {
-			expect(Words.list().length).to.equal(3) 
-		})
-	})
-	describe(".set", function () {
-		it("should add a word w/o returning a value", function () {
-			expect(Words.set('hay')).to.equal(undefined)
-		})
-	})
+describe('WordFreq', function () {
+    it('Words should be an instance of WordFreq', function () {
+        expect(Words instanceof WordFreq).to.equal(true)
+    })
+    describe('.get', function () {
+        it('should not find a word it was not initialized with', function () {
+            expect(Words.get('thomas')).not.to.equal(true)
+        })
+        it('should return the correct word value', function () {
+            expect(Words.get('sup')['word']).to.equal('sup')
+        })
+    })
+    describe('.list', function () {
+        it('should have the correct length', function() {
+            expect(Words.list().length).to.equal(3) 
+        })
+    })
+    describe('.set', function () {
+        it('should add a word w/o returning a value', function () {
+            expect(Words.set('hay')).to.equal(undefined)
+        })
+    })
 
 })
