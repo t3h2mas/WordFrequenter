21ca32d83caa24a31a4715c2ad5d9ffaf1d7c43f
Thomas Noe <thomas.noe@madwiremedia.com>
2018-05-02 20:36:17 -0600

    update test language

--- test/test.js
+++ test/test.js
@@ -3,32 +3,27 @@ const WordFreq = require('../wordfrequenter.js')
 const list = ['esp', 'jtruant', 'sup']
 const Words = new WordFreq(list)
 
-describe("Make the app", function () {
+describe("WordFreq", function () {
 	it("Words should be an instance of WordFreq", function () {
 		expect(Words instanceof WordFreq).to.equal(true)
 	})
-})
+	describe(".get", function () {
+		it("should not find a word it was not initialized with", function () {
+			expect(Words.get('thomas')).not.to.equal(true)
+		})
+		it("should return the correct word value", function () {
+			expect(Words.get('sup')['word']).to.equal('sup')
+		})
+	})
+	describe(".list", function () {
+		it("should have the correct length", function() {
+			expect(Words.list().length).to.equal(3) 
+		})
+	})
+	describe(".set", function () {
+		it("should add a word w/o returning a value", function () {
+			expect(Words.set('hay')).to.equal(undefined)
+		})
+	})
 
-describe("Get Test", function () {
-	it("Words.get()", function () {
-		expect(Words.get('thomas')).not.to.equal(true)
-	})
-})
-
-describe("Get List", function () {
-	it("List should return a list of Word objects", function() {
-		expect(Words.list().length).to.equal(3) // from list /\
-	})
-})
-
-describe("Set -> Add a new entry", function () {
-	it("Should add an entry, but return undefined", function () {
-		expect(Words.set('hay')).to.equal(undefined)
-	})
-	it("Should still be an instance of WordFreq", function () {
-		expect(Words instanceof WordFreq).to.equal(true)
-	})
-	it("Should return a result after .get('hay')", function () {
-		expect(Words.get('hay')['word']).to.equal('hay')
-	})
 })
