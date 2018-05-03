014b9d5cfb8a7f56699ca89715cd320bd644d56f
Thomas Noe <thomas.noe@madwiremedia.com>
2018-05-02 20:31:07 -0600

    update code style

--- test/test.js
+++ test/test.js
@@ -1,34 +1,34 @@
-var expect = require('chai').expect
-var WordFreq = require('../wordfrequenter.js');
-var list = ['esp', 'jtruant', 'sup'];
-var Words = new WordFreq(list);
+const expect = require('chai').expect
+const WordFreq = require('../wordfrequenter.js')
+const list = ['esp', 'jtruant', 'sup']
+const Words = new WordFreq(list)
 
 describe("Make the app", function () {
 	it("Words should be an instance of WordFreq", function () {
-		expect(Words instanceof WordFreq).to.equal(true);
-	});
-});
+		expect(Words instanceof WordFreq).to.equal(true)
+	})
+})
 
 describe("Get Test", function () {
 	it("Words.get()", function () {
-		expect(Words.get('thomas')).not.to.equal(true);
-	});
-});
+		expect(Words.get('thomas')).not.to.equal(true)
+	})
+})
 
 describe("Get List", function () {
 	it("List should return a list of Word objects", function() {
-		expect(Words.list().length).to.equal(3); // from list /\
-	});
-});
+		expect(Words.list().length).to.equal(3) // from list /\
+	})
+})
 
 describe("Set -> Add a new entry", function () {
 	it("Should add an entry, but return undefined", function () {
-		expect(Words.set('hay')).to.equal(undefined);
-	});
+		expect(Words.set('hay')).to.equal(undefined)
+	})
 	it("Should still be an instance of WordFreq", function () {
-		expect(Words instanceof WordFreq).to.equal(true);
-	});
+		expect(Words instanceof WordFreq).to.equal(true)
+	})
 	it("Should return a result after .get('hay')", function () {
-		expect(Words.get('hay')['word']).to.equal('hay');
-	});
-});
+		expect(Words.get('hay')['word']).to.equal('hay')
+	})
+})
--- wordfrequenter.js
+++ wordfrequenter.js
@@ -1,42 +1,42 @@
-var _ = require('underscore');
+const _ = require('underscore')
 
 
 function Word(word) {
-	word = word || '';
-	this.word = word;
-	_.defaults(this, {count: 1});
+	word = word || ''
+	this.word = word
+	_.defaults(this, {count: 1})
 }
 
 function WordFreq(start) {
-	start = start || [];
-	this.vals = [];
+	start = start || []
+	this.vals = []
 	// add any items from start into the list, we assume that
 	// the list looks like ['word', ...] and counts will be defaulted
 	start.forEach(function (word) {
 		this.set(word)
-	}, this);
+	}, this)
 }
 
 // TODO: fix WordFreq -> Word
 WordFreq.prototype.set = function(word) {
 	found = this.get(word)
 	if (!found) { // need to test for duplicates so we don't overwrite
-		w = new Word(word);
-		this.vals.push(w);
+		w = new Word(word)
+		this.vals.push(w)
 	} else {
-		++found['count'];
+		++found['count']
 	}
 }
 
 WordFreq.prototype.get = function(word) {
-	var resp = _.find(this.vals, function(entry) { return entry['word'] == word; });
-	return resp;
+	var resp = _.find(this.vals, function(entry) { return entry['word'] == word; })
+	return resp
 }
 
 // TODO: fix sorting
 WordFreq.prototype.list = function () {
-	var resp = this.vals.sort(function (a, b) { return a['count'] > b['count']; });
-	return resp;
+	var resp = this.vals.sort(function (a, b) { return a['count'] > b['count']; })
+	return resp
 }
 
-module.exports = WordFreq;
+module.exports = WordFreq
